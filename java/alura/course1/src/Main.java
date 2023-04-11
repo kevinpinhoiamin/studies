aac1a9d6e49e503e90e42eafdaf3eb1d166427df
Kevin Pinho Iamin <kiamin@1worldsync.com>
2023-04-11 08:48:14 -0300

    course continuation.

--- java/alura/course1/src/Main.java
+++ java/alura/course1/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is the Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int releaseYear = 2022;
+        System.out.println("Release year: " + releaseYear);
+        boolean includedInPlan = true;
+        double grade = 8.1;
+
+        // Average grade calculated from 3 grades
+        double averageGrade = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(averageGrade);
+
+        String synopsis = """
+                Top Gun Movie
+                Adventure movie with heartthrob from the 80's
+                Very good!
+                Release year
+                """ + releaseYear;
+        System.out.println(synopsis);
+
+        int classification = (int) (averageGrade / 2);
+        System.out.println(classification);
     }
 }
