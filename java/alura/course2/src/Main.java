d8f9896ab12774edb42345ccb93c35677e8f4017
Kevin Pinho Iamin <kiamin@1worldsync.com>
2023-04-19 20:29:27 -0300

    course continuation

--- java/alura/course2/src/Main.java
+++ java/alura/course2/src/Main.java
@@ -1,19 +1,23 @@
+import br.com.alura.course2.models.Movie;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Movie myMovie = new Movie();
-        myMovie.name = "Godfather";
-        myMovie.releaseYear = 1972;
-        myMovie.durationInMinutes = 175;
+        myMovie.setName("Godfather");
+        myMovie.setReleaseYear(1972);
+        myMovie.setDurationInMinutes(175);
 
         myMovie.displayTechnicalSheet();
         myMovie.makeRating(8);
         myMovie.makeRating(5);
         myMovie.makeRating(10);
-        System.out.println(myMovie.sumOfRatings);
-        System.out.println(myMovie.totalRatings);
+        System.out.println("Total ratings: " + myMovie.getTotalRatings());
         System.out.println(myMovie.getAverageRatings());
+        // myMovie.sumOfRatings = 1;
+        // myMovie.totalRatings = 10;
+        // System.out.println(myMovie.getAverageRatings());
 
     }
 
