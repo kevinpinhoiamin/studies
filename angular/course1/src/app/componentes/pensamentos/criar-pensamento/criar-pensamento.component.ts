8d0685c056919e89669b69cc4bfda10794c40f7c
Kevin Pinho Iamin <kevinpinhoiamin@gmail.com>
2024-05-12 19:27:33 -0300

    Iniciado o curso de Angular.

---
+++ angular/course1/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-criar-pensamento',
+    templateUrl: './criar-pensamento.component.html',
+    styleUrls: ['./criar-pensamento.component.css'],
+})
+export class CriarPensamentoComponent implements OnInit {
+    pensamento = {
+        id: '1',
+        conteudo: 'Apredendo Angular',
+        autoria: 'Dev',
+        modelo: '',
+    };
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    criarPensamento(): void {
+        alert('Novo pensamento criado!');
+    }
+
+    cancelar(): void {
+        alert('Ação cancelada!');
+    }
+}
