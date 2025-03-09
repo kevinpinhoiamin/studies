53059900730b5a6017e0a9ca4fe31a82fcb220d0
Kevin Pinho Iamin <kevinpinhoiamin@gmail.com>
2025-03-09 18:47:21 -0300

    Continuação do curso

--- angular/udemy/course1/01-starting-project/src/app/app.component.ts
+++ angular/udemy/course1/01-starting-project/src/app/app.component.ts
@@ -2,18 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { User } from './user/user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS;
+  users: User[] = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser(): User | undefined {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string): void {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
--- angular/udemy/course1/01-starting-project/src/app/user/user.component.ts
+++ angular/udemy/course1/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -7,16 +8,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(): string {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser(): void {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
