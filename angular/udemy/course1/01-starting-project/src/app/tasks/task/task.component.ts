148092f5679ec3385190b37d3f46bcb7bad1de9d
Kevin Pinho Iamin <kevinpinhoiamin@gmail.com>
2025-03-23 18:36:30 -0300

    continuação do curso

--- angular/udemy/course1/01-starting-project/src/app/tasks/task/task.component.ts
+++ angular/udemy/course1/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCompleteTask(): void {
+    this.tasksService.removeTask(this.task.id);
+  }
 }
--- angular/udemy/course1/01-starting-project/src/app/user/user.component.ts
+++ angular/udemy/course1/01-starting-project/src/app/user/user.component.ts
@@ -1,14 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath(): string {
